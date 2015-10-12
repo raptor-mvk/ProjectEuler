4f773ab9f3c3b80d89f7f2967b64f156780dadaa
raptor-mvk <m.v.kamorin@gmail.com>
2015-10-12 17:41:04 +0300

    Added task 5

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ Solutions to problems from http://projecteuler.net
 # Topics
 
 * Elementary
-  * [1](https://projecteuler.net/problem=1)
+  * [1](https://projecteuler.net/problem=1),
+    [4](https://projecteuler.net/problem=4)
 * Fibonnaci numbers
   * [2](https://projecteuler.net/problem=2)
+* Prime numbers
+  * [3](https://projecteuler.net/problem=3),
+    [5](https://projecteuler.net/problem=5)
---
+++ src/euler/task5.clj
@@ -0,0 +1,21 @@
+(ns
+  ^{:author raptor_MVK}
+  euler.task5
+  (:use clojure.test)
+  (:use tools.seqs))
+
+(defn min-multiple
+  "Given N, returns the smallest positive number that is evenly divisible by all of the
+  numbers from 1 to N"
+  [n]
+  (let [less-n (fn [coll] (take-while #(<= % n) coll))
+        pow-iter (fn [n] (iterate #(* % n) n))
+        prime-factors (less-n (primes-seq))
+        prime-powers (map #(last (less-n (pow-iter %))) prime-factors)]
+    (reduce *' prime-powers)))
+
+(deftest test1 (is (= (min-multiple 10) 2520)))
+
+(deftest test2 (is (= (min-multiple 20) 232792560)))
+
+(time (run-tests 'euler.task5))
