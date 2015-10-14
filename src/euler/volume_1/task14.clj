b788408893c22e2a40e4b3975ebc8b7f96d1da55
raptor-mvk <m.v.kamorin@gmail.com>
2015-10-14 22:29:27 +0300

    Added faster solution to task 14

--- src/euler/volume_1/task14.clj
+++ src/euler/volume_1/task14.clj
@@ -9,9 +9,21 @@
 (defn max-collatz-seq
   "Given N, returns starting number, under N, produces the longest Collatz sequence"
   [n]
-  (first (reduce #(if (> (last %2) (last %1)) %2 %1)
-           (map #(vector % (count (collatz-seq %))) (range 2 (inc n))))))
+  (let [collatz-len (fn collatz-len [n lens]
+                       (let [len (get-in lens [n])]
+                         (if (nil? len)
+                           (let [new-n (if (odd? n)
+                                         (inc (*' n 3))
+                                         (quot n 2))
+                                 new-lens (collatz-len new-n lens)]
+                             (assoc-in new-lens [n] (inc (get-in new-lens [new-n]))))
+                           (assoc-in lens [n] len))))]
+    (loop [ns (range 2 n)
+          lens {1 1}]
+      (if (empty? ns)
+        (key (apply max-key val lens))
+        (recur (rest ns) (collatz-len (first ns) lens))))))
 
-(deftest test2 (is (= (max-collatz-seq 1000000) 837799)))
+(deftest test1 (is (= (max-collatz-seq 1000000) 837799)))
 
 (time (run-tests 'euler.volume_1.task14))
